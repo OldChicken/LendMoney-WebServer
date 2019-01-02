96bc4b09cc4ff284b286324b260fb756e6858228
JohnLebert <1242027557@qq.com>
2019-01-02 17:19:32 +0800

    new_function
    描述：redis模块功能+生成验证码功能
    时间：2019.1.2
    提交人：lch

---
+++ userauthor/getcode.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#coding:utf-8
+import asyncio
+ 
+from urllib.parse import urlencode
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+import json
+ 
+from tornado import web
+from forms import SmsForm
+from random import choice
+import os.path
+import torndb
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options 
+
+#生成注册验证码
+class GetCode(tornado.web.RequestHandler):
+    def create_code(self):
+        # 创建四位数短信验证码
+        seeds = '01234566789'
+        code = []
+        for i in range(4):
+            code.append(choice(seeds))
+        return ''.join(code)
+    
+    def post(self):
+        phone = self.get_body_argument("user_phone")
+        tp = self.get_body_argument("type")
+        code = self.create_code()
+        text = {'user_phone':phone,'type':'您的验证码为' + code}
+        resp = json.dumps(text)
+        self.redis_conn.set('{}_{}'.format(mobile, code), 1, 5*60)
+        self.write(resp)
+        self.set_header('Content-Type','application/json;charset=utf-8')
